883f4abc77c55ab0b3c25005ee03dd9fcb7f9ba8
lihongfei <2529574569@qq.com>
2024-07-16 22:36:15 +0800

    优化consul配置文件及其测试效果

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/Main8001.java
@@ -3,6 +3,7 @@ package com.atguigu.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -12,7 +13,8 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan("com.atguigu.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
-@EnableDiscoveryClient
+@EnableDiscoveryClient //服务注册和发现
+@RefreshScope // 动态刷新
 public class Main8001 {
 
     public static void main(String[] args) {
