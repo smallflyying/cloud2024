e6a94ff6760543a3faa414c244299c221a8ad64a
lihongfei <2529574569@qq.com>
2024-07-21 12:25:14 +0800

    隔离(BulkHead)

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderCircuitController.java
@@ -1,11 +1,15 @@
 package com.atguigu.cloud.controller;
 
 import com.atguigu.cloud.apis.PayFeignApi;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author LiHongFei
@@ -29,4 +33,43 @@ public class OrderCircuitController {
         // 这里是容错处理逻辑，返回备用结果
         return "myCircuitFallback, 系统繁忙，请稍后再试-----/(●'◡'●)/~~";
     }
+
+    /**
+     *(船的)舱壁,隔离
+     * @param id
+     * @return
+     */
+    /*@GetMapping(value = "/feign/pay/bulkhead/{id}")
+    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadFallback",type = Bulkhead.Type.SEMAPHORE)
+    public String myBulkhead(@PathVariable("id") Integer id)
+    {
+        return payFeignApi.myBulkhead(id);
+    }
+    public String myBulkheadFallback(Throwable t) {
+        return "myBulkheadFallback，隔板超出最大数量限制，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+    }*/
+
+    /**
+     *(船的)舱壁,隔离 threadPool
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/feign/pay/bulkhead/{id}")
+    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadPoolFallback",type = Bulkhead.Type.THREADPOOL)
+    public CompletableFuture<String> myBulkheadTHREADPOOL(@PathVariable("id") Integer id) {
+        System.out.println(Thread.currentThread().getName() + "\t" + "---开始进入");
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t" + "---准备离开");
+
+        return CompletableFuture.supplyAsync(() -> payFeignApi.myBulkhead(id) + "\t" + "Bulkhead.Type.THREADPOOL");
+    }
+    public CompletableFuture<String> myBulkheadPoolFallback(Integer id, Throwable t) {
+//        return "myBulkheadFallback，隔板超出最大数量限制，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+        return CompletableFuture.supplyAsync(() -> "Bulkhead.Type.THREADPOOL，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~");
+    }
+
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayCircuitController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayCircuitController.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 public class PayCircuitController {
 
     //=============Resilience4j Circuit Breaker 的例子
-    @GetMapping(value = "/pay/circuit/{id}")
+    /*@GetMapping(value = "/pay/circuit/{id}")
     public String myCircuit(@PathVariable("id") Integer id) {
         if (id == -4) throw new RuntimeException("circuit id 不能为负数");
 
@@ -28,5 +28,23 @@ public class PayCircuitController {
             }
         }
         return "Hello, circuit! inputId: " + id + " \t " + IdUtil.simpleUUID();
+    }*/
+
+    //=========Resilience4j bulkhead 的例子
+    @GetMapping(value = "/pay/bulkhead/{id}")
+    public String myBulkhead(@PathVariable("id") Integer id)
+    {
+        if(id == -4) throw new RuntimeException("----bulkhead id 不能-4");
+
+        if(id == 9999)
+        {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "Hello, bulkhead! inputId:  "+id+" \t " + IdUtil.simpleUUID();
     }
 }
