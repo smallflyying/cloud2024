b6ee68acbf28f9efa63a6e8a812f56d9f47d7ced
lihongfei <2529574569@qq.com>
2024-07-22 21:50:29 +0800

    gateway模块创建

--- cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
+++ cloud-api-commons/src/main/java/com/atguigu/cloud/apis/PayFeignApi.java
@@ -62,4 +62,28 @@ public interface PayFeignApi {
      */
     @GetMapping(value = "/pay/ratelimit/{id}")
     public String myRatelimit(@PathVariable("id") Integer id);
+
+    /**
+     * Micrometer(Sleuth)进行链路监控的例子
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/pay/micrometer/{id}")
+    public String myMicrometer(@PathVariable("id") Integer id);
+
+
+    /**
+     * GateWay进行网关测试案例01
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/pay/gateway/get/{id}")
+    public ResultData getById(@PathVariable("id") Integer id);
+
+    /**
+     * GateWay进行网关测试案例02
+     * @return
+     */
+    @GetMapping(value = "/pay/gateway/info")
+    public ResultData<String> getGatewayInfo();
 }
---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderGateWayController.java
@@ -0,0 +1,9 @@
+package com.atguigu.cloud.controller;
+
+/**
+ * @author LiHongFei
+ * @version 1.0
+ * @since 2024/7/22
+ */
+public class OrderGateWayController {
+}
