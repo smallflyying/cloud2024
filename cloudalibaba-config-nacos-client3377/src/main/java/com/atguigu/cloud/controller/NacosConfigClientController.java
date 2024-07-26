9271b56c275eb4f91b734f133af4cf589d0f0ae6
lihongfei <2529574569@qq.com>
2024-07-26 21:24:08 +0800

    nacos模块-配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/cloud/controller/NacosConfigClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LiHongFei
+ * @version 1.0
+ * @since 2024/7/25
+ */
+@RestController
+@RefreshScope //在控制器类加入@RefreshScope注解使当前类下的配置支持Nacos的动态刷新功能。
+public class NacosConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
